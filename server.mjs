5b3f61ca73cbfc552985726076f6dcf0f2b79c14
Piotr Kamecki <piotrkamecki8@gmail.com>
2022-11-29 12:18:58 +0100

    replit4

--- server.mjs
+++ server.mjs
@@ -1,13 +1,20 @@
+/* eslint-disable no-unused-vars */
 /* global require, process */
-const jsonServer = require('json-server');
+// eslint-disable-next-line no-unused-vars
+import path from 'path';
+import jsonServer from 'json-server';
+
 const server = jsonServer.create();
-const router = jsonServer.router('dist/db/app.json');
+const router = jsonServer.router(path.join('dist', 'db', 'app.json'));
 const middlewares = jsonServer.defaults({
   static: 'dist',
   noCors: true
 });
-
 const port = process.env.PORT || 3131;
+
 server.use(middlewares);
 server.use(router);
+
 server.listen(port);
+
+export default server;
